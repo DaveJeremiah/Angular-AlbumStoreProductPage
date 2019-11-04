6c418925275b1ce9c28057e3688305f3f785b8a6
Dave Jeremiah <davejayden49@gmail.com>
2019-11-04 21:02:38 +0300

    Completed Module

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -4,8 +4,8 @@ import { Product } from '../product';
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './products-list.component.html',
-  styleUrls: ['./products-list.component.css']
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
 products: Product[]
